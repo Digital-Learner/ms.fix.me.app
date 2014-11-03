188e569fc5b44919df3d71fa5b9e0f5ab0217c6c
Digital-Learner <digital.learning.journeys@gmail.com>
2014-11-03 02:24:03 +0000

    Add validations for image attachments

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,14 @@ class Product < ActiveRecord::Base
   def self.random_best_sellers(number)
   	find(:all, :limit => number, :order => 'RAND()')
   end
+
+  validate  :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
